586baf8a078e40c8a7d3cce98b81519fea404985
Jhonny Test <iriyaz93@gmail.com>
2015-11-02 18:03:37 +0530

    Initial Commit

---
+++ Weather5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather5
+//
+//  Created by Riyaz Ahamed on 02/11/15.
+//  Copyright (c) 2015 SimplyApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
